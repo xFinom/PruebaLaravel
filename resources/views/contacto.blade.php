2506f7cf4c93ffbe6c982312a33a13e0d52f47e8
Alvaro Romero <alvaro_romero35@hotmail.com>
2023-09-04 16:45:23 -0600

    ruta y vista del contacto

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Formulario de Contacto</h1>
+    <form action="" method>
+        <input type="email" name="correo" id="correo">
+    </form>
+</body>
+</html>
